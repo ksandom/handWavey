903c9dc74145b04aa648b482631084287ecb7bfb
Kevin Sandom <kjsandom@gmail.com>
2021-12-09 16:42:23 +0100

    Resize a MovingMean on the fly

--- src/main/java/dataCleaner/MovingMean.java
+++ src/main/java/dataCleaner/MovingMean.java
@@ -1,9 +1,9 @@
 package dataCleaner;
 
 public class MovingMean {
-    private int size;
-    private int position;
-    private double[] data;
+    private int size = 0;
+    private int position = 0;
+    private double[] data = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
     public MovingMean(int size, double seedValue) {
         this.size = size;
@@ -38,8 +38,22 @@ public class MovingMean {
 
     private void step() {
         this.position++;
-        if (this.position > this.size) {
+        if (this.position > this.size - 1) {
             this.position = 0;
+        }
+    }
+
+    public void resize(int newSize) {
+        /* This is not completely correct because old values will no longer decay out of view. Instead their average is spread across the entire window, and that average will fade out of view.
+
+        To do this correctly, it would need to effectively grow the buffer as new data comes in. To do this easily while maintaining the order of the data to that it decays correctly, it would need to defrag the data back to 0 each time the resize is run. Shrink could do it in a single step, but would still need the defrag so that the correct data is truncated.
+
+        I need this to be able to be run many times per second on low end hardware without hogging resources. Therefore the following approximation is used instead. */
+        if (newSize != this.size) {
+            double currentMean = get();
+            this.position = 0;
+            this.size = newSize;
+            seed(currentMean);
         }
     }
 }
