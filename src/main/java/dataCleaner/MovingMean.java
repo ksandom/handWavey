069562712a66bcadc7320e8ba21e4b8d683cf960
Kevin Sandom <kjsandom@gmail.com>
2021-12-09 16:42:23 +0100

    Resize a MovingMean on the fly

--- src/main/java/dataCleaner/MovingMean.java
+++ src/main/java/dataCleaner/MovingMean.java
@@ -1,22 +1,22 @@
 package dataCleaner;
 
+import java.util.Arrays;
+
 public class MovingMean {
-    private int size;
-    private int position;
-    private double[] data;
+    private int size = 0;
+    private int position = 0;
+    private double[] data = new double[4096];
 
     public MovingMean(int size, double seedValue) {
         this.size = size;
         this.position = 0;
-
+        
         seed(seedValue);
     }
 
     // Make all values the same as a starting point. This is great for transitioning from a mode that doesn't use this instance of the moving mean. That way the result is immediately at the expected, and can continue from there.
     public void seed(double value) {
-        for (int i=0; i<this.size; i++) {
-            this.data[i] = value;
-        }
+        Arrays.fill(this.data, 0, this.size, value);
     }
 
     // Put in a data point.
@@ -38,8 +38,22 @@ public class MovingMean {
 
     private void step() {
         this.position++;
-        if (this.position > this.size) {
+        if (this.position > this.size - 1) {
             this.position = 0;
+        }
+    }
+
+    public void resize(int newSize) {
+        /* This is not completely correct because old values will no longer decay out of view. Instead their average is spread across the entire window, and that average will fade out of view.
+
+        To do this correctly, it would need to effectively grow the buffer as new data comes in. To do this easily while maintaining the order of the data to that it decays correctly, it would need to defrag the data back to 0 each time the resize is run. Shrink could do it in a single step, but would still need the defrag so that the correct data is truncated.
+
+        I need this to be able to be run many times per second on low end hardware without hogging resources. Therefore the following approximation is used instead. */
+        if (newSize != this.size) {
+            double currentMean = get();
+            this.position = 0;
+            this.size = newSize;
+            seed(currentMean);
         }
     }
 }
--- src/test/java/dataCleaner/TestMovingMean.java
+++ src/test/java/dataCleaner/TestMovingMean.java
@@ -51,4 +51,27 @@ class TestMovingMean {
         this.movingMean.set(-2);
         assertEquals(0, this.movingMean.get());
     }
+
+    @Test
+    public void testResize() {
+        this.movingMean.set(1);
+        this.movingMean.set(1);
+        this.movingMean.set(2);
+        this.movingMean.set(2);
+        assertEquals(1.5, this.movingMean.get());
+        
+        this.movingMean.resize(8);
+        assertEquals(1.5, this.movingMean.get());
+        
+        // TODO Add tests that would show the weaknesses of the current implementation. Specifically around which old values get evited.
+        this.movingMean.set(4);
+        this.movingMean.set(4);
+        this.movingMean.set(4);
+        this.movingMean.set(4);
+        assertEquals(2.75, this.movingMean.get());
+        
+        // This is where the current implementation really shows it's weakness. If it was 100% correct, the value would be 4 (the last 4 entries added were 4, so that would be all that is left.). But because we take the average of everything, and seed the whole array with that, it comes to 2.75. This will need to be fixed at some point for statistical accuracy. But for now this is good enough for what we need.
+        this.movingMean.resize(4);
+        assertEquals(2.75, this.movingMean.get());
+    }
 }
