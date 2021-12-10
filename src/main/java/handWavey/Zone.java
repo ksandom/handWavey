0563a9e84cc1ac33e54dc0854d2c843f8c2c5584
Kevin Sandom <kjsandom@gmail.com>
2021-12-10 14:32:57 +0100

    Add Zone for tracking things like depth

---
+++ src/main/java/handWavey/Zone.java
@@ -0,0 +1,37 @@
+package handWavey;
+
+public class Zone {
+    private double zBegin;
+    private double zEnd;
+    private double zSpan;
+    
+    private int movingMeanBegin;
+    private int movingMeanEnd;
+    private int movingMeanSpan;
+    
+    public Zone(double zBegin, double zEnd, int movingMeanBegin, int movingMeanEnd) {
+        this.zBegin = zBegin;
+        this.zEnd = zEnd;
+        this.zSpan = this.zEnd - this.zBegin;
+        
+        this.movingMeanBegin = movingMeanBegin;
+        this.movingMeanEnd = movingMeanEnd;
+        this.movingMeanSpan = this.movingMeanEnd - this.movingMeanBegin;
+    }
+    
+    public double getDepthPercent(double z) {
+        if (z < zBegin) z = zBegin;
+        if (z > zEnd) z = zEnd;
+        
+        double rebased = z - this.zBegin;
+        
+        return rebased / this.zSpan;
+    }
+    
+    public int getMovingMeanWidth(double z) {
+        double scaledValue = movingMeanSpan * getDepthPercent(z);
+        int movingMeanWidth = (int) Math.round(scaledValue + movingMeanBegin);
+        
+        return movingMeanWidth;
+    }
+}
---
+++ src/test/java/handWavey/TestZone.java
@@ -0,0 +1,49 @@
+package handWavey;
+
+import handWavey.Zone;
+import handWavey.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+// TODO We need to handle the case of the hand no longer being seen while a mouse down state is present. The mouse should be released so that the desktop returns to being usable.
+
+
+class TestZone {
+    Zone zone;
+
+    @BeforeEach
+    void setUp() {
+        this.zone = new Zone(-150, 50, 1, 11);
+    }
+
+    @AfterEach
+    void destroy() {
+        this.zone = null;
+    }
+
+    @Test
+    public void testPosition() {
+        assertEquals(0, this.zone.getDepthPercent(-180));
+        assertEquals(0, this.zone.getDepthPercent(-150));
+        assertEquals(0.1, this.zone.getDepthPercent(-130));
+        assertEquals(0.5, this.zone.getDepthPercent(-50));
+        assertEquals(1, this.zone.getDepthPercent(50));
+        assertEquals(1, this.zone.getDepthPercent(150));
+    }
+
+    @Test
+    public void testMovingMeanWidth() {
+        assertEquals(1, this.zone.getMovingMeanWidth(-150));
+        assertEquals(2, this.zone.getMovingMeanWidth(-130));
+        assertEquals(2, this.zone.getMovingMeanWidth(-121));
+        assertEquals(3, this.zone.getMovingMeanWidth(-120));
+        assertEquals(3, this.zone.getMovingMeanWidth(-119));
+        assertEquals(10, this.zone.getMovingMeanWidth(30));
+        assertEquals(11, this.zone.getMovingMeanWidth(49));
+        assertEquals(11, this.zone.getMovingMeanWidth(50));
+        assertEquals(11, this.zone.getMovingMeanWidth(51));
+    }
+}
