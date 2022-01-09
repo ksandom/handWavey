bf64eefd31a35a90bd0c1186e97a176bf5a405e9
Kevin Sandom <kjsandom@gmail.com>
2022-01-09 19:27:00 +0100

    Add Changed for tracking changes

---
+++ src/main/java/dataCleaner/Changed.java
@@ -0,0 +1,63 @@
+package dataCleaner;
+
+public class Changed {
+    private String strCurrent = "";
+    private int intCurrent = 0;
+    
+    private String strPrevious = "";
+    private int intPrevious = 0;
+    
+    private Boolean changed = false;
+    
+    public Changed(String value) {
+        set(value);
+        set(value); // Set a secont time to remove the changed state.
+    }
+    
+    public Changed(int value) {
+        set(value);
+        set(value); // Set a secont time to remove the changed state.
+    }
+    
+    public void set(String value) {
+        if (value != this.strCurrent) {
+            this.strPrevious = this.strCurrent;
+            this.strCurrent = value;
+            this.changed = true;
+        } else {
+            this.strPrevious = value;
+            this.changed = false;
+        }
+    }
+    
+    public void set(int value) {
+        if (value != this.intCurrent) {
+            this.intPrevious = this.intCurrent;
+            this.intCurrent = value;
+            this.changed = true;
+        } else {
+            this.intPrevious = value;
+            this.changed = false;
+        }
+    }
+    
+    public Boolean hasChanged() {
+        return this.changed;
+    }
+    
+    public String fromStr() {
+        return this.strPrevious;
+    }
+    
+    public int fromInt() {
+        return this.intPrevious;
+    }
+    
+    public String toStr() {
+        return this.strCurrent;
+    }
+    
+    public int toInt() {
+        return this.intCurrent;
+    }
+}
---
+++ src/test/java/dataCleaner/TestChanged.java
@@ -0,0 +1,84 @@
+package dataCleaner;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+import dataCleaner.Changed;
+
+class TestChanged {
+    Changed changedStr;
+    Changed changedInt;
+
+    @BeforeEach
+    void setUp() {
+        this.changedStr = new Changed("");
+        this.changedInt = new Changed(0);
+    }
+
+    @AfterEach
+    void destroy() {
+        this.changedStr = null;
+        this.changedInt = null;
+    }
+
+    @Test
+    public void testSequence() {
+         // Starting state.
+         assertEquals(false, this.changedStr.hasChanged());
+         assertEquals(false, this.changedInt.hasChanged());
+         assertEquals("", this.changedStr.toStr());
+         assertEquals(0, this.changedInt.toInt());
+         assertEquals("", this.changedStr.fromStr());
+         assertEquals(0, this.changedInt.fromInt());
+         
+         
+         // Change it.
+         this.changedStr.set("something else");
+         this.changedInt.set(1);
+         
+         assertEquals(true, this.changedStr.hasChanged());
+         assertEquals(true, this.changedInt.hasChanged());
+         assertEquals("something else", this.changedStr.toStr());
+         assertEquals(1, this.changedInt.toInt());
+         assertEquals("", this.changedStr.fromStr());
+         assertEquals(0, this.changedInt.fromInt());
+         
+         
+         // Set the same values again. We should see no change.
+         this.changedStr.set("something else");
+         this.changedInt.set(1);
+         
+         assertEquals(false, this.changedStr.hasChanged());
+         assertEquals(false, this.changedInt.hasChanged());
+         assertEquals("something else", this.changedStr.toStr());
+         assertEquals(1, this.changedInt.toInt());
+         assertEquals("something else", this.changedStr.fromStr());
+         assertEquals(1, this.changedInt.fromInt());
+         
+         
+         // A new change.
+         this.changedStr.set("another value");
+         this.changedInt.set(-1);
+         
+         assertEquals(true, this.changedStr.hasChanged());
+         assertEquals(true, this.changedInt.hasChanged());
+         assertEquals("another value", this.changedStr.toStr());
+         assertEquals(-1, this.changedInt.toInt());
+         assertEquals("something else", this.changedStr.fromStr());
+         assertEquals(1, this.changedInt.fromInt());
+         
+         
+         // Changing back do the starting values should be a normal change.
+         this.changedStr.set("");
+         this.changedInt.set(0);
+         
+         assertEquals(true, this.changedStr.hasChanged());
+         assertEquals(true, this.changedInt.hasChanged());
+         assertEquals("", this.changedStr.toStr());
+         assertEquals(0, this.changedInt.toInt());
+         assertEquals("another value", this.changedStr.fromStr());
+         assertEquals(-1, this.changedInt.fromInt());
+    }
+}
