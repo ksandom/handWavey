8490956f49b4a13d920ee5e7ba24fc7006959e3e
Kevin Sandom <kjsandom@gmail.com>
2022-01-19 18:26:15 +0100

    Improve unit tesing for output protection

--- src/test/java/mouseAndKeyboardOutput/TestOutputProtection.java
+++ src/test/java/mouseAndKeyboardOutput/TestOutputProtection.java
@@ -25,76 +25,43 @@ class TestOutputProtection {
         this.output = null;
     }
 
-    @Test
-    public void testGetDesktopResolution() {
-        Dimension dim = this.output.getDesktopResolution();
-        assertEquals(123, dim.width);
-        assertEquals(321, dim.height);
-    }
-    
-    @Test
-    public void testSetPosition() {
-        this.output.setPosition(456, 654);
-        assertEquals(456, this.output.testInt("posX"));
-        assertEquals(654, this.output.testInt("posY"));
-    }
-    
-    @Test
-    public void testClick() {
-        assertEquals(0, this.output.testInt("clicked"));
-        this.output.click("left");
-        assertEquals(InputEvent.BUTTON1_MASK, this.output.testInt("lastMouseButton"));
-        assertEquals(1, this.output.testInt("clicked"));
-    }
-    
-    @Test
-    public void testDoubleClick() {
-        assertEquals(0, this.output.testInt("clicked"));
-        this.output.doubleClick("middle");
-        assertEquals(InputEvent.BUTTON2_MASK, this.output.testInt("lastMouseButton"));
-        assertEquals(1, this.output.testInt("clicked"));
-    }
-    
-    @Test
-    public void testMouseDown() {
-        this.output.mouseDown("left");
-        assertEquals(InputEvent.BUTTON1_MASK, this.output.testInt("lastMouseButton"));
-        this.output.mouseDown("right");
-        assertEquals(InputEvent.BUTTON3_MASK, this.output.testInt("lastMouseButton"));
-    }
+    // TODO Find a better way of testing this.
+//     @Test
+//     public void testMouseDown() {
+//         this.output.mouseDown("left");
+//         assertEquals(InputEvent.BUTTON1_MASK, this.output.testInt("lastMouseButton"));
+//         this.output.mouseDown("right");
+//         assertEquals(InputEvent.BUTTON3_MASK, this.output.testInt("lastMouseButton"));
+//         this.output.mouseDown("left");
+//         
+//         // The button is already down. So the protection should stop it from going down again.
+//         assertEquals(InputEvent.BUTTON3_MASK, this.output.testInt("lastMouseButton"));
+//     }
     
     @Test
     public void testMouseUp() {
-        this.output.mouseUp("left");
-        assertEquals(0, this.output.testInt("lastMouseButton"));
         this.output.mouseDown("left");
         assertEquals(InputEvent.BUTTON1_MASK, this.output.testInt("lastMouseButton"));
         this.output.mouseUp("left");
         assertEquals(InputEvent.BUTTON1_MASK, this.output.testInt("lastMouseButton"));
-        this.output.mouseDown("right");
-        assertEquals(InputEvent.BUTTON3_MASK, this.output.testInt("lastMouseButton"));
+        
+        // We have not yet pressed down the right button. So it should not press.
         this.output.mouseUp("right");
-        assertEquals(InputEvent.BUTTON3_MASK, this.output.testInt("lastMouseButton"));
+        assertEquals(InputEvent.BUTTON1_MASK, this.output.testInt("lastMouseButton"));
     }
     
-    @Test
-    public void testScroll() {
-        assertEquals(0, this.output.testInt("scroll"));
-        this.output.scroll(1);
-        assertEquals(1, this.output.testInt("scroll"));
-        this.output.scroll(3);
-        assertEquals(4, this.output.testInt("scroll"));
-        this.output.scroll(-2);
-        assertEquals(2, this.output.testInt("scroll"));
-    }
-    
-    @Test
-    public void testKeyDown() {
-        this.output.keyDown("ctrl");
-        assertEquals(KeyEvent.VK_CONTROL, this.output.testInt("lastKey"));
-        this.output.keyDown("alt");
-        assertEquals(KeyEvent.VK_ALT, this.output.testInt("lastKey"));
-    }
+    // TODO Find a better way of testing this.
+//     @Test
+//     public void testKeyDown() {
+//         this.output.keyDown("ctrl");
+//         assertEquals(KeyEvent.VK_CONTROL, this.output.testInt("lastKey"));
+//         this.output.keyDown("alt");
+//         assertEquals(KeyEvent.VK_ALT, this.output.testInt("lastKey"));
+//         
+//         // We have already pressed the ctrl key, so it should not get pressed again.
+//         this.output.keyDown("ctrl");
+//         assertEquals(KeyEvent.VK_ALT, this.output.testInt("lastKey"));
+//     }
     
     @Test
     public void testKeyUp() {
@@ -102,14 +69,9 @@ class TestOutputProtection {
         assertEquals(KeyEvent.VK_CONTROL, this.output.testInt("lastKey"));
         this.output.keyUp("ctrl");
         assertEquals(KeyEvent.VK_CONTROL, this.output.testInt("lastKey"));
-        this.output.keyDown("alt");
-        assertEquals(KeyEvent.VK_ALT, this.output.testInt("lastKey"));
+        
+        // We have not yet pressed down the alt key, so it should not get pressed.
         this.output.keyUp("alt");
-        assertEquals(KeyEvent.VK_ALT, this.output.testInt("lastKey"));
-    }
-    
-    @Test
-    public void testGetKeysIKnow() {
-        assertEquals(3, this.output.getKeysIKnow().size());
+        assertEquals(KeyEvent.VK_CONTROL, this.output.testInt("lastKey"));
     }
 }
---
+++ src/test/java/mouseAndKeyboardOutput/TestProtector.java
@@ -0,0 +1,59 @@
+package mouseAndKeyboardOutput;
+
+import mouseAndKeyboardOutput.*;
+import debug.Debug;
+
+import java.awt.Dimension;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestProtector {
+    private Protector protector;
+    
+    @BeforeEach
+    void setUp() {
+        this.protector = new Protector(new Debug(0, "Protector"), new NullOutput());
+    }
+
+    @AfterEach
+    void destroy() {
+        this.protector = null;
+    }
+
+    @Test
+    public void testSingleSequence() {
+        assertEquals(false, this.protector.isDown("thing"));
+        assertEquals(true, this.protector.isUp("thing"));
+        
+        this.protector.setDown("thing");
+        assertEquals(true, this.protector.isDown("thing"));
+        assertEquals(false, this.protector.isUp("thing"));
+        
+        this.protector.setUp("thing");
+        assertEquals(false, this.protector.isDown("thing"));
+        assertEquals(true, this.protector.isUp("thing"));
+    }
+    
+    @Test
+    public void testMultipleSequence() {
+        assertEquals(false, this.protector.isDown("thing1"));
+        assertEquals(false, this.protector.isDown("thing2"));
+        assertEquals(false, this.protector.isDown("thing3"));
+        
+        this.protector.setDown("thing1");
+        assertEquals(true, this.protector.isDown("thing1"));
+        assertEquals(false, this.protector.isDown("thing2"));
+        assertEquals(false, this.protector.isDown("thing3"));
+        
+        this.protector.setDown("thing2");
+        assertEquals(true, this.protector.isDown("thing1"));
+        assertEquals(true, this.protector.isDown("thing2"));
+        assertEquals(false, this.protector.isDown("thing3"));
+        
+        assertEquals(2, this.protector.getDownItems().size());
+    }
+}
