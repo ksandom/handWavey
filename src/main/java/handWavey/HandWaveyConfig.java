b22e21e6ace4843321dd441834fde19105ad57da
Kevin Sandom <kjsandom@gmail.com>
2024-02-26 15:31:01 +0100

    Reduce wait for scroll to become active

--- src/main/java/handWavey/HandWaveyConfig.java
+++ src/main/java/handWavey/HandWaveyConfig.java
@@ -900,7 +900,7 @@ public class HandWaveyConfig {
 
         macrosGroup.newItem(
             "yankScroll-enter",
-            "cancelAllDelayedDos();lockCursor();allowWheelClicks();setSlot(\"3\", \"do-scroll\");lockTaps(\"primary\");unlockTaps(\"primary\" , \"800\");delayedDo(\"do-scroll\", \"1500\");",
+            "cancelAllDelayedDos();lockCursor();allowWheelClicks();setSlot(\"3\", \"do-scroll\");lockTaps(\"primary\");unlockTaps(\"primary\" , \"800\");delayedDo(\"do-scroll\", \"1200\");",
             "Yank scrolling is the grab to scroll, where you need to yank it to get it started. The -enter macro gets it set up.");
 
         macrosGroup.newItem(
